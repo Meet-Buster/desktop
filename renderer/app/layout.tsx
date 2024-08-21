24b13f2d929f07c155e0af9d88c042fba3c8ef1e
alieldeba <alieldeba375@gmail.com>
2024-08-21 23:48:06 +0300

    build: nextron boilerplate with typescript, tailwind & shadcn

---
+++ renderer/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Cairo } from "next/font/google";
+import React from "react";
+import "./globals.css";
+// import { Toaster } from "sonner";
+
+const cairo = Cairo({ subsets: ["arabic"] });
+
+export const metadata: Metadata = {
+  title: "Buster",
+  //   description: "حساباتي هو تطبيق لإدارة جميع التحويلات لحسابك.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={cairo.className}>
+        <main className="container">{children}</main>
+        {/* <Toaster richColors /> */}
+      </body>
+    </html>
+  );
+}
---
+++ renderer/app/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Button } from "../components/ui/button";
+
+function page() {
+  return <Button variant="outline">Button</Button>;
+}
+
+export default page;
